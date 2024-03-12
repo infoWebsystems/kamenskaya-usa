2ecb923797ec60ae1c07d636cbf7f261737085e0
user <user@users-MacBook-Pro.local>
2024-03-12 13:54:28 +0200

    theme

---
+++ assets/wss.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    // Wrapping all submenu items in a level 4 heading
+    // const submenuItems = document.querySelectorAll(".navigation_submenu-item");
+    //
+    // submenuItems.forEach((item) => {
+    //     const anchorTag = item.querySelector("a");
+    //     if (anchorTag) {
+    //         const h4Tag = document.createElement("h4");
+    //         h4Tag.appendChild(anchorTag.cloneNode(true));
+    //         item.innerHTML = "";
+    //         item.appendChild(h4Tag);
+    //     }
+    // });
+});
